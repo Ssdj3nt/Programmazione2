e188c944785ad20442c00deed4755278c9e91546
Francesco Porritiello <92294163+Ssdj3nt@users.noreply.github.com>
2022-08-11 18:02:26 +0200

    Update DecimalToAnyWComma.c

--- DecimalToAnyWComma.c
+++ DecimalToAnyWComma.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 #include <math.h>
-
 void main()
 {
     float num,fnum,m;
-    int dnum,base,a[100],i;
+    int lenb,lenf,dnum,base,a[100],i;
 
     printf("Inserisci un numero razionale:");
     scanf("%f",&num);
-    dnum=(int)num;
-    fnum=num-dnum;
     printf("Inserisci la base da convertire:");
     scanf("%d",&base);
 
-    for(i=0;i<((sizeof(float)*8)-(int)log2(dnum)+1);i++)
-    {
-        m=fnum*base;
-        a[i]=(int)m;
-        fnum=m;
-    }
+    dnum=(int)num;
+    fnum=num-(float)dnum;
 
+    if(dnum>0)
+        lenb=(int)log2(dnum)+1;
+    else
+        lenb=1;
+
+    lenf=sizeof(float)*8;
+
+    printf("\nParte binaria dopo la virgola:\n");
+
+    for(i=lenb+1;i<lenf;i++)//Viene lasciato lo spazio nell'array sufficiente per inserire i bit della parte intera prima della parte dopo la virgola;
+    {
+        m=fnum*(float)base;
+        a[i]=(int)m;
+        printf("%d",a[i]);
+        fnum=m-(int)m;
+    }
 }
