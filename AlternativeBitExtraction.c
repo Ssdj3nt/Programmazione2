1edb05c60bc8ed3bee862d3961cf1761ac4227ee
Francesco Porritiello <92294163+Ssdj3nt@users.noreply.github.com>
2022-09-01 14:49:11 +0200

    Create AlternativeBitExtraction.c

---
+++ AlternativeBitExtraction.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+    int x,a[8];
+    short i;
+    puts("Inserisci un numero float:");
+    scanf("%d",&x);
+
+    for(i=7;i>=0;i--)
+    {
+        a[i]=(char)(1&x);
+        x=x>>1;
+        printf("%1d",a[i]);
+    }
+}
