4fa7655bce6469e9f987972cd1a1ab815903e243
Francesco Porritiello <92294163+Ssdj3nt@users.noreply.github.com>
2023-01-27 23:56:59 +0100

    Update StringMatching.c

--- StringMatching.c
+++ StringMatching.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+
 int main() {
-    char *str = "ita ITA ia ita B";
+    char *str = "it ti IT i iat ita iut i t a ita";
     char *sub = "ita";
     char *occur;
     int count = 0;
     occur = strstr(str, sub);
-    while (occur != 'NULL') {
-        printf("Occorrenza trovata alla posizione %ld\n", occur - str);
+    while (occur != NULL) {
+        printf("Occorrenza trovata alla posizione %d\n", occur - str);
         count++;
         occur = strstr(occur + 1, sub);
     }
