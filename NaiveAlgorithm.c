a56640893a0f6f5db9d9447be8859edf0ab35ea9
Francesco Porritiello <92294163+Ssdj3nt@users.noreply.github.com>
2023-01-30 10:20:40 +0100

    Update NaiveAlgorithm.c

--- NaiveAlgorithm.c
+++ NaiveAlgorithm.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
-void main()
+int main()
 {
-    char *testo="Francesco abcd CR Francesco francesco";
-    char *chiave="Francesco",*occorrenza,*pos=testo;
-    int lent,lenc,n=0;
-    lent=strlen(testo);
-    lenc=strlen(chiave);
-
+    char *t="aa a aaa a aaaa aaa aa aa";
+    char *p="aa";
+    char *ptr,*te;
+    int cnt=0,len_t,len_p;
+    len_t= strlen(t);
+    len_p= strlen(p);
+    te=t;
     do
     {
-     occorrenza= strstr(pos,chiave);
-     if(occorrenza!=NULL)
-     {
-         n++;
-         pos=occorrenza+lenc;
-     }
-    } while (pos<testo+lent && occorrenza!=0);
-    printf("Occorrenze sottostringa nel testo:%d",n);
+        ptr= strstr(te,p);
+        if(ptr != NULL)
+        {
+            cnt++;
+            te=ptr+len_p;
+        }
+    }while(te < t+len_t && ptr != NULL);
+    printf("cnt:%d",cnt);
 }
