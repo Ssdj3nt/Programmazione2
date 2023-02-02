84f667370efb5bdded38d8db30945de421fb5944
Francesco Porritiello <92294163+Ssdj3nt@users.noreply.github.com>
2023-02-02 16:45:33 +0100

    Update 2DArray-rowmaniupulation(del).c

--- 2DArray-rowmaniupulation(del).c
+++ 2DArray-rowmaniupulation(del).c
@@ -1,57 +1,54 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-void remove_row(int** matrix, int row, int rows, int columns);
-int main() {
-    int rows, columns;
-    printf("Inserisci il numero di righe e colonne nella matrice: ");
-    scanf("%d%d", &rows, &columns);
+#include "time.h"
+#include "stdlib.h"
+#include "string.h"
 
-    int** matrix = (int**)malloc(rows * sizeof(int*));
-    for (int i = 0; i < rows; i++) {
-        matrix[i] = (int*)malloc(columns * sizeof(int));
+int main(void)
+{
+    int **matrix,n,m,del,i,j;
+    srand((unsigned)time(NULL));
+
+    puts("Digita il numero di righe ed il numero di colonne della matrice:");
+    scanf("%d\t%d",&n,&m);
+
+    matrix=(int**)calloc(n,sizeof(int*));
+    for(i=0;i<n;i++){
+        matrix[i]=(int*)calloc(m,sizeof(int));
     }
-
-    printf("Inserisci gli elementi della matrice:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            scanf("%d", &matrix[i][j]);
-        }
-    }
-
-    int row;
-    printf("Inserisci l'indice della riga da eliminare: ");
-    scanf("%d", &row);
-
-    remove_row(matrix, row, rows, columns);
-
-    // Deallocare la memoria assegnata alla matrice
-    for (int i = 0; i < rows; i++) {
-        free(matrix[i]);
-    }
-    free(matrix);
-
-    return 0;
-}
-void remove_row(int** matrix, int row, int rows, int columns) {
-    // Calcolare la dimensione in byte di una riga
-    int row_size = columns * sizeof(int);
-
-    // Spostare le righe successive verso l'alto per sovrascrivere la riga da eliminare
-    memmove(&matrix[row], &matrix[row + 1], (rows - row - 1) * row_size);
-
-    // Liberare la memoria non più utilizzata
-    memset(&matrix[rows - 1], 0, row_size);
-
-    // Ridurre il numero di righe nella matrice
-    rows--;
-
-    // Stampa la nuova matrice
-    printf("Nuova matrice:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            printf("%d ", matrix[i][j]);
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            matrix[i][j]=rand()%100;
+            printf("%d\t",matrix[i][j]);
         }
         printf("\n");
     }
+
+    while(1){
+        puts("Indica l'indice della riga da eliminare:");
+        scanf("%d",&del);
+        if(del>n){
+            puts("L'indice non fa parte della matrice.\nRiprova.");
+        }
+        if(del<=n){
+            break;
+        }
+    }
+
+    memmove(&matrix[del],&matrix[del+1],(n-del-1)*m*sizeof(int));
+    memset(&matrix[n-1],0,m*sizeof(int));
+    n--;
+    matrix=(int**)realloc(matrix,n*sizeof(int*));
+
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            printf("%d\t",matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(i=0;i<n;i++)
+    {
+        free(matrix[i]);
+    }
+    free(matrix);
 }
