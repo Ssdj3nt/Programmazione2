fdb8185ecea088fc493a06bb259e32e2bf0622f9
Ssdj3nt <92294163+Ssdj3nt@users.noreply.github.com>
2023-02-21 11:08:08 +0100

    Update Stack.c

--- Stack.c
+++ Stack.c
@@ -1,24 +1,97 @@
-#include <stdio.h>
-void inverti(char*,int);
-#define len 5
-int main(void)
-{
-    char stack[5]={'a','b','c','d','e'};
-    int i;
-    puts("Lo stack:");
-    for(i=0;i<len;i++){
-        printf("%c\t",stack[i]);
-    }
-    printf("\n");
-    inverti(stack,len);
+#include <stdio.h> 
+#include <stdlib.h>
+
+//Se mi serve globale posso decommentare la linea successiva
+//#define LUN 26
+
+void push(char, char *, short *);
+void pop(char *, char *, short *);
+void inverti_array(char *, short);
+
+void main() { 
+
+    //65 é il numero ascii da dove partono le lettere maiuscole
+
+    short lun;
+    char *lista;
+
+    char risposta;
+
+
+
+
+    do
+    {   puts("\n/*********************************************/\n");
+
+        printf("Inserisci la lunghzza dell'array di caratteri: ");
+        scanf("%d", &lun);
+        int k = 0;
+        
+            lista = (char *)calloc(lun, sizeof(char));
+
+            for (short i = 65; i < 65+lun; i++)
+            {   
+                lista[k++] = (char)i;
+            }
+    
+            printf("\nArray di size %d PRIMA dell'inversione:\n\n", lun);
+            for (short i = 0; i < lun; i++)
+            {
+                printf("a[%d]\t=\t%c\n", i+1, *(lista + i));
+            }
+
+            inverti_array(lista, lun);
+
+            printf("\nArray di size %d DOPO dell'inversione:\n\n", lun);
+            for (short i = 0; i < lun; i++)
+            {
+                printf("a[%d]\t=\t%c\n", i+1, *(lista + i));
+            }
+
+
+        printf("\nVuoi riprovare? (s/n): ");
+        fflush(stdin);
+        scanf("%c", &risposta);
+        
+        free (lista);
+
+    } while (risposta == 's');
+    
 }
-void inverti(char *a,int l){
-    char buffer[l];
-    int j,k=l-1;
-    for(j=0;j<l;j++){
-        buffer[k--]=a[j];
+
+
+
+void inverti_array(char *lista, short lun){
+
+    short head = -1;
+
+    char *p_temp;
+
+    p_temp = (char *)calloc(lun, sizeof(char));
+
+    for (short i = 0; i < lun; i++)
+    {
+        push(*(lista + i), p_temp, &head);
     }
-    for(j=0;j<l;j++){
-        printf("%c\t",buffer[j]);
+    
+    for (short i = 0; i < lun; i++)
+    {
+        pop(&lista[i], p_temp, &head);
     }
+    
+    free(p_temp);
+
+}
+
+
+void push(char elem, char *stack, short *head){
+
+    *(stack + (++*head)) = elem;
+
+}
+
+void pop(char *elem, char *stack, short *head){
+
+    *elem = *(stack + (*head)--);
+
 }
